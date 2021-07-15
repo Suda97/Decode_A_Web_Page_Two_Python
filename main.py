97c477a3f2f56c1de781762c69b399a7b8e716a0
MCSuda <suder.michal97@gmail.com>
2021-07-15 14:20:52 +0200

    Small update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture'
-source = requests.get(base_url)
-soup = BeautifulSoup(source.text, 'html.parser')
-all = soup.find_all("p", {"class": "has-dropcap"}) + soup.find_all("p", {"class": "paywall"})
+if __name__ == '__main__':
+    base_url = 'https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture'
+    source = requests.get(base_url)
+    soup = BeautifulSoup(source.text, 'html.parser')
+    all = soup.find_all("p", {"class": "has-dropcap"}) + soup.find_all("p", {"class": "paywall"})
 
-for elem in all:
-    print(elem.text)
+    for elem in all:
+        print(elem.text)
